9a2a5eeff4b10291e8c09c609b6846dbeb3ca37f
Thiago Delgado Pinto <thiago_dp@yahoo.com.br>
2026-06-05 03:26:29 -0300

    Update examples

--- examples/deno/example-web.ts
+++ examples/deno/example-web.ts
@@ -1,6 +1,6 @@
 // deno --allow-import example-web.ts
 
-import countryToCurrency, { Currencies, Countries } from "https://unpkg.com/country-to-currency";
+import countryToCurrency, { Currencies, Countries } from "https://unpkg.com/country-to-currency@3";
 
 const currency: Currencies = 'BRL';
 const country: Countries = 'BR';
--- examples/typescript-pkg/example.ts
+++ examples/typescript-pkg/example.ts
@@ -1,6 +1,8 @@
 // pnpm exec tsc && node example.js
 
-import countryToCurrency, { Currencies, Countries } from "country-to-currency";
+import type { Currencies, Countries } from "country-to-currency";
+import countryToCurrency from "country-to-currency";
+
 
 const currency: Currencies = 'BRL';
 const country: Countries = 'BR';
